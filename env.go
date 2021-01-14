8bbebdee3c66e66c65350e5245ab4c79bbb9ad6a
open6g <75805865+open6g@users.noreply.github.com>
2021-01-14 13:24:09 -0500

    v2
    With environment variables and interface information

--- env.go
+++ env.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"strings"
 	"os"
+	"net"
+	
+	
 )
 
 func main() {
 	fmt.Println("Environment Variables")
 	fmt.Println(strings.Repeat("=", 21))
-	fmt.Println("PATH =", os.Getenv("PATH"))
+	// fmt.Println("PATH =", os.Getenv("PATH"))
 	for _, lines := range os.Environ() {
 		fmt.Println(lines)      	
     	}
+	fmt.Println()
+	
+	fmt.Println("Network Interface Information")
+	fmt.Println(strings.Repeat("=", 29))
+	
+	interfaces, _ := net.Interfaces()
+   	for _, interf := range interfaces {
+      		fmt.Print("Index:", interf.Index, " ")
+      		fmt.Print("MTU:", interf.MTU, " ")
+		fmt.Print("Name:", interf.Name, " ")
+		fmt.Print("HardwareAddr:", interf.HardwareAddr, " ")
+		fmt.Print("Flags:", interf.Flags, " ")
+		addrs,_ := interf.Addrs()
+		fmt.Println("IP Address:", addrs, " ")
+	}
 }
