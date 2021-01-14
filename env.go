5c9f164bf2540a7fe33f005014d89e530582b2cf
open6g <75805865+open6g@users.noreply.github.com>
2021-01-14 12:11:09 -0500

    v1.1
    Added environment variables

--- env.go
+++ env.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"strings"
+	"os"
 )
 
 func main() {
 	fmt.Println("Environment Variables")
 	fmt.Println(strings.Repeat("=", 21))
+	fmt.Println("PATH =", os.Getenv("PATH"))
+	for _, lines := range os.Environ() {
+		fmt.Println(lines)      	
+    	}
 }
